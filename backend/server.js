2e289c291f261a73b872272f93dd6a905b0d27ee
AchiniWijayathunge <100922967+AchiniWijayathunge@users.noreply.github.com>
2023-01-16 23:01:03 +0530

    firstcommit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express =  require ('express')
+const mongoose = require ('mongoose')
+const workoutRoutes = require("./routes/workouts");
+require('dotenv').config()
+const url = 'mongodb://127.0.0.1/MEANapp'
+
+const app = express()
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open' ,() => {
+    console.log('connected to the database...')
+} )
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+//routes
+app.use("/api/workouts",workoutRoutes);
+
+app.listen(9000, () =>{
+    console.log('Server started')
+})
