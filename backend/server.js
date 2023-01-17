a18402b669dc17a5aac0f0c0ed8f434dc949c7a2
AchiniWijayathunge <100922967+AchiniWijayathunge@users.noreply.github.com>
2023-01-17 18:37:08 +0530

    setup the backend

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
-const express =  require ('express')
-const mongoose = require ('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
-require('dotenv').config()
-const url = 'mongodb://127.0.0.1/MEANapp'
+require("dotenv").config();
+const url = "mongodb://127.0.0.1/MEANapp";
 
-const app = express()
+const app = express();
 
-mongoose.connect(url, {useNewUrlParser:true})
-const con = mongoose.connection
+mongoose.connect(url, { useNewUrlParser: true });
+const con = mongoose.connection;
 
-con.on('open' ,() => {
-    console.log('connected to the database...')
-} )
+con.on("open", () => {
+  console.log("connected to the database...");
+});
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 //routes
-app.use("/api/workouts",workoutRoutes);
+app.use("/api/workouts", workoutRoutes);
 
-app.listen(4000, () =>{
-    console.log('Server started')
-})
+app.listen(4000, () => {
+  console.log("Server started");
+});
